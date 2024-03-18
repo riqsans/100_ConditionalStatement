4c253f440d192e38c07bead18b90ec957fbc28cf
riqsans <thrq88@gmail.com>
2024-03-18 14:33:04 +0700

    PerulanganFor.cpp

---
+++ Pertemuan  keempat PEMDAS/PerulanganFor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int i;
+
+    for(i = 0;i<5;i++){
+        
+        cout << i << " : " << "Thoriq" << endl;
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukkan Nama =";
+        cin >> arr[i];
+    }
+
+    for (i = 0; i<5;i++){
+        cout << "Nama ke -" << i << " : " << arr[i] << endl;
+    }
+}
